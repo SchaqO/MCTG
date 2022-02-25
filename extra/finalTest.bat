a650363e055f575667b84381c265c923ef8d8765
SchaqO <shako-1998@hotmail.com>
2022-02-25 06:11:52 +0100

    added some files

---
+++ extra/finalTest.bat
@@ -0,0 +1,104 @@
+@echo off
+
+REM --------------------------------------------------
+REM Monster Trading Cards Game
+REM --------------------------------------------------
+title Monster Trading Cards Game
+echo CURL Testing for Monster Trading Cards Game
+echo.
+
+REM --------------------------------------------------
+echo 1) Create Users (Registration)
+REM Create User
+curl -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
+echo.
+curl -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"altenhof\", \"Password\":\"markus\"}"
+echo.
+curl -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
+echo.
+
+echo should fail:
+curl -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
+echo.
+curl -X POST http://localhost:10001/users --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"different\"}"
+echo. 
+echo.
+
+REM --------------------------------------------------
+echo 2) Login Users
+curl -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"daniel\"}"
+echo.
+curl -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"altenhof\", \"Password\":\"markus\"}"
+echo.
+curl -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"admin\",    \"Password\":\"istrator\"}"
+echo.
+
+echo should fail:
+curl -X POST http://localhost:10001/sessions --header "Content-Type: application/json" -d "{\"Username\":\"kienboec\", \"Password\":\"different\"}"
+echo.
+echo.
+
+REM --------------------------------------------------
+echo 14) edit user data
+echo.
+curl -X GET http://localhost:10001/users/kienboec --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+curl -X GET http://localhost:10001/users/altenhof --header "Authorization: Basic altenhof-mtcgToken"
+echo.
+curl -X PUT http://localhost:10001/users/kienboec --header "Content-Type: application/json" --header "Authorization: Basic kienboec-mtcgToken" -d "{\"Name\": \"Kienboeck\",  \"Bio\": \"me playin...\", \"Image\": \":-)\"}"
+echo.
+curl -X PUT http://localhost:10001/users/altenhof --header "Content-Type: application/json" --header "Authorization: Basic altenhof-mtcgToken" -d "{\"Name\": \"Altenhofer\", \"Bio\": \"me codin...\",  \"Image\": \":-D\"}"
+echo.
+curl -X GET http://localhost:10001/users/kienboec --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+curl -X GET http://localhost:10001/users/altenhof --header "Authorization: Basic altenhof-mtcgToken"
+echo.
+echo.
+echo should fail:
+curl -X GET http://localhost:10001/users/altenhof --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+curl -X GET http://localhost:10001/users/kienboec --header "Authorization: Basic altenhof-mtcgToken"
+echo.
+curl -X PUT http://localhost:10001/users/kienboec --header "Content-Type: application/json" --header "Authorization: Basic altenhof-mtcgToken" -d "{\"Name\": \"Hoax\",  \"Bio\": \"me playin...\", \"Image\": \":-)\"}"
+echo.
+curl -X PUT http://localhost:10001/users/altenhof --header "Content-Type: application/json" --header "Authorization: Basic kienboec-mtcgToken" -d "{\"Name\": \"Hoax\", \"Bio\": \"me codin...\",  \"Image\": \":-D\"}"
+echo.
+curl -X GET http://localhost:10001/users/someGuy  --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+echo.
+
+REM --------------------------------------------------
+echo 15) stats
+curl -X GET http://localhost:10001/stats --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+curl -X GET http://localhost:10001/stats --header "Authorization: Basic altenhof-mtcgToken"
+echo.
+echo.
+
+REM --------------------------------------------------
+echo 16) scoreboard
+curl -X GET http://localhost:10001/score --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+echo.
+
+REM --------------------------------------------------
+echo 18) Stats 
+echo kienboec
+curl -X GET http://localhost:10001/stats --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+echo altenhof
+curl -X GET http://localhost:10001/stats --header "Authorization: Basic altenhof-mtcgToken"
+echo.
+echo.
+
+REM --------------------------------------------------
+echo 19) scoreboard
+curl -X GET http://localhost:10001/score --header "Authorization: Basic kienboec-mtcgToken"
+echo.
+echo.
+
+echo end...
+
+REM this is approx a sleep 
+ping localhost -n 100 >NUL 2>NUL
+@echo on
