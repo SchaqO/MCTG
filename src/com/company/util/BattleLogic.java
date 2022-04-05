eb137250ccdc573c31247b79019fc34046d7b757
SchaqO <shako-1998@hotmail.com>
2022-04-05 19:13:22 +0200

    started coding on the battle

--- src/com/company/util/BattleLogic.java
+++ src/com/company/util/BattleLogic.java
@@ -1,5 +1,7 @@
 package com.company.util;
 
+import com.company.db.CardDB;
+import com.company.db.UserDB;
 import com.company.model.Battle;
 import com.company.model.Deck;
 import com.company.model.User;
@@ -15,9 +17,40 @@ public class BattleLogic {
 
     public Battle battleStart(Battle battle){
 
-        Deck deckA = battle.getPlayer1().getDeck();
-        battle.getPlayer2();
 
+    ArrayList<AbstractCard> deckA = new ArrayList<>();
+    ArrayList<AbstractCard> deckB = new ArrayList<>();
+
+    User userA = battle.getPlayer1();
+    User userB = battle.getPlayer2();
+    UserDB userDB = new UserDB();
+    CardDB cardDB = new CardDB();
+
+    if(userA.getDeck() == null){
+        return null;
+    }
+    if(userB.getDeck() == null){
+        return null;
+    }
+
+        deckA.add(cardDB.getItemByToken(userA.getDeck().getCardId1()));
+        deckA.add(cardDB.getItemByToken(userA.getDeck().getCardId2()));
+        deckA.add(cardDB.getItemByToken(userA.getDeck().getCardId3()));
+        deckA.add(cardDB.getItemByToken(userA.getDeck().getCardId4()));
+
+
+        deckB.add(cardDB.getItemByToken(userA.getDeck().getCardId1()));
+        deckB.add(cardDB.getItemByToken(userA.getDeck().getCardId2()));
+        deckB.add(cardDB.getItemByToken(userA.getDeck().getCardId3()));
+        deckB.add(cardDB.getItemByToken(userA.getDeck().getCardId4()));
+
+
+        String nameA = deckB.get(0).getCardName().toString();
+        String nameB = deckA.get(0).getCardName().toString();
+
+        if(nameA == "DRAGON" && nameB == "ORK"){
+            //logic
+        }
 
 
 
